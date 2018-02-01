b2277ffbbda2491b765c9c0b2c2146b219bd9396
Nobel1893 <32608939+Nobel1893@users.noreply.github.com>
2018-02-01 14:42:44 +0200

    Create program.py

---
+++ program.py
@@ -0,0 +1,10 @@
+
+##unsorted list
+mylist=[5,6,23,0,2,1,77,33,2]
+##call built in function sorted
+newlist=sorted(mylist)
+##showing the result
+print ' mylist ' 
+print  mylist
+print 'sorted list '
+print newlist
